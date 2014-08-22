bf154298f5e3efac7c2765bfd0d51d3f2768c10b
Jordan Smith <jtsu888@gmail.com>
2014-08-22 00:24:43 -0400

    adds passwords back for heroku

--- feedsAndPasswords.js
+++ feedsAndPasswords.js
@@ -1,10 +1,9 @@
-//sample object for feeds
 var instaFeeds = {
 	"ithaca": {
 		name: "ithaca",
 		data: require('./routes/instagram/ithaca'),
-		clientID: "*****",
-		clientSecret: "*****",
+		clientID: "77298706852440abb566e7f08cfa29ae",
+		clientSecret: "75610934c202477aacab453904000376",
 		tags: [
 			"ithaca",
 			"ithacacollege",
@@ -15,8 +14,8 @@ var instaFeeds = {
 	"psu": {
 		name: "psu",
 		data: require('./routes/instagram/psu'),
-		clientID: "*****",
-		clientSecret: "*****",
+		clientID: "d83c63e8a8834fea8c3fc616734be9a1",
+		clientSecret: "7f5b6eb75c464ddba7432f4ebcc48a14",
 		tags: [
 		    "psu", 
 		    "pennstate", 
@@ -26,8 +25,8 @@ var instaFeeds = {
 	"harvard": {
 		name: "harvard",
 		data: require('./routes/instagram/harvard'),
-		clientID: "*****",
-		clientSecret: "*****",
+		clientID: "f6d061861e4f41e6bc5a56ea370b1828",
+		clientSecret: "128ba7e2f7d14e3d9b0fd90e628ac79a",
 		tags: [
 			"harvard",
 			"harvarduniversity"
